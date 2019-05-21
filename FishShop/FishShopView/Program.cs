8a57b0a2033ab19bd7ee83b07e73edd1c8df3538
A Dyrkin <dyrno4kin@gmail.com>
2019-05-21 17:28:13 +0400

    Lab_7

--- FishShop/FishShopView/Program.cs
+++ FishShop/FishShopView/Program.cs
@@ -1,6 +1,3 @@
-using FishShopServiceDAL.Interfaces;
-using FishShopServiceImplementDataBase;
-using FishShopServiceImplementDataBase.Implementations;
 using System;
 using System.Windows.Forms;
 
@@ -19,7 +16,6 @@ namespace FishShopView
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
-
         }
     }
 }
