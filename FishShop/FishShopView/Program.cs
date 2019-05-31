466607f5e8c14c3acb2a7fb0ec7342338d98c807
Dyrno4kin <dyrno4kin@gmail.com>
2019-05-31 15:12:16 +0400

    Lab_3WPF

--- FishShop/FishShopView/Program.cs
+++ FishShop/FishShopView/Program.cs
@@ -1,6 +1,9 @@
 ﻿using FishShopServiceDAL.Interfaces;
 using FishShopServiceImplement.Implementations;
+using FishShopServiceImplementDataBase;
+using FishShopServiceImplementDataBase.Implementations;
 using System;
+using System.Data.Entity;
 using System.Windows.Forms;
 using Unity;
 using Unity.Lifetime;
@@ -18,20 +21,26 @@ namespace FishShopView
             var container = BuildUnityContainer();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(container.Resolve<FormMain>());
+            Application.Run(container.Resolve<FormMain>());
+
         }
 
         public static IUnityContainer BuildUnityContainer()
         {
             var currentContainer = new UnityContainer();
+            currentContainer.RegisterType<DbContext, FishDbContextWPF>(new
+           HierarchicalLifetimeManager());
             currentContainer.RegisterType<ICustomerService, CustomerServiceDB>(new
            HierarchicalLifetimeManager());
             currentContainer.RegisterType<IIngredientService, IngredientServiceDB>(new
            HierarchicalLifetimeManager());
             currentContainer.RegisterType<ICanFoodService, CanFoodServiceDB>(new
            HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IStockService, StockServiceDB>(new
+           HierarchicalLifetimeManager());
             currentContainer.RegisterType<IMainService, MainServiceDB>(new
            HierarchicalLifetimeManager());
+
             return currentContainer;
         }
     }
