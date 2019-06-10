21a82ed4e022f32ed6a3b7585303b07b2af59813
Dyrno4kin <dyrno4kin@gmail.com>
2019-06-10 18:31:40 +0400

    Lab_4WPF

---
+++ FishShop/FishShopWPFView/FormCustomerOrder.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using FishShopServiceDAL.BindingModels;
+using FishShopServiceDAL.Interfaces;
+using Microsoft.Reporting.WinForms;
+using Microsoft.Win32;
+using Unity;
+u
+
+namespace FishShopWPFView
+{
+    /// <summary>
+    /// Логика взаимодействия для FormCustomerOrder.xaml
+    /// </summary>
+    public partial class FormCustomerOrder : Window
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+
+        private readonly IReportService service;
+
+        public FormCustomerOrder(IReportService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void buttonMake_Click(object sender, EventArgs e)
+        {
+            if (dateTimePickerFrom.SelectedDate >= dateTimePickerTo.SelectedDate)
+            {
+                MessageBox.Show("Дата начала должна быть меньше даты окончания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                reportViewer.LocalReport.ReportEmbeddedResource = "CarpenterWorkshopWpf.ReportCustomersOrders.rdlc";
+                ReportParameter parameter = new ReportParameter("ReportParameterPeriod",
+                                            "c " + dateTimePickerFrom.SelectedDate.ToString() +
+                                            " по " + dateTimePickerTo.SelectedDate.ToString());
+                reportViewer.LocalReport.SetParameters(parameter);
+
+
+                var dataSource = service.GetCustomerOrders(new ReportBindingModel
+                {
+                    DateFrom = dateTimePickerFrom.SelectedDate,
+                    DateTo = dateTimePickerTo.SelectedDate
+                });
+                ReportDataSource source = new ReportDataSource("DataSetOrders", dataSource);
+                reportViewer.LocalReport.DataSources.Add(source);
+
+
+
+
+                reportViewer.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void buttonToPdf_Click(object sender, EventArgs e)
+        {
+            if (dateTimePickerFrom.SelectedDate >= dateTimePickerTo.SelectedDate)
+            {
+                MessageBox.Show("Дата начала должна быть меньше даты окончания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "pdf|*.pdf"
+            };
+            if (sfd.ShowDialog() == true)
+            {
+                try
+                {
+                    service.SaveCustomerOrders(new ReportBindingModel
+                    {
+                        FileName = sfd.FileName,
+                        DateFrom = dateTimePickerFrom.SelectedDate,
+                        DateTo = dateTimePickerTo.SelectedDate
+                    });
+                    MessageBox.Show("Выполнено", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+    }
+}
+}
